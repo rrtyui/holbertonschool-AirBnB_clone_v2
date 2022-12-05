a113279540cc419fd8b5ac542beff1d39f2a9b06
rrtyui <marcelo6549@gmail.com>
2022-12-04 21:52:41 -0300

    fix 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
+"""Write a script that starts a Flask web application"""
 
-from markupsafe import escape
 
 from flask import Flask, render_template
 
@@ -9,39 +8,49 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
+def hello_hbnb():
+    """when request route "/" displays “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """generic comment"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cmessage(text):
-    return 'C {}'.format(escape(text.replace("_", " ")))
+@app.route("/c/<txt>", strict_slashes=False)
+def c_(txt):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return "C {}".format(txt)
 
 
-@app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pymessage(text):
-    return 'Python {}'.format(escape(text.replace("_", " ")))
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<txt>", strict_slashes=False)
+def python_(txt="is cool"):
+    """generic comment"""
+    txt = txt.replace("_", " ")
+    return "Python {}".format(txt)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def vernumber(n):
-    return '{} is a number'.format(escape(n))
+@app.route("/number/<int:my_num>", strict_slashes=False)
+def number(my_num):
+    """generic comment"""
+    return "{} is a number".format(my_num)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def page(n):
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:my_num>", strict_slashes=False)
+def my_number(my_num):
+    """generic comment"""
+    return render_template('5-number.html', num=my_num)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def spage(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+def odd_or_even(n):
+    """generic comment"""
+    return render_template('6-number_odd_or_even.html', num=n)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
